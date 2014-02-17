e5e66c21cd3ed4641b9fb6863401097e9613d29b
Oleg Nemanov <lego12239@yandex.ru>
2014-02-17 19:51:49 +0400

    Version 0.4.0.

--- htmltmpl.js
+++ htmltmpl.js
@@ -1,5 +1,5 @@
 /*
-  Version 0.3.0
+  Version 0.4.0
 
   This library is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
