6a84f688c2d02ba4cd4131c237c1d429fed4c842
Oleg Nemanov <lego12239@yandex.ru>
2014-02-17 19:50:48 +0400

    Add a download uri to the source code.

--- htmltmpl.js
+++ htmltmpl.js
@@ -13,6 +13,8 @@
 
   You should have received a copy of the GNU Lesser General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  https://github.com/lego12239/htmltmpl
 */
 {
     "use strict";
