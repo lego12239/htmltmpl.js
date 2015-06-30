83fbf697eff61fa700302fad4c4cb428fea92d2c
Oleg Nemanov <lego12239@yandex.ru>
2015-06-30 16:07:07 +0300

    Add a library version into core.js.

--- htmltmpl/core.js
+++ htmltmpl/core.js
@@ -1,5 +1,7 @@
 /* htmltmpl.core | Version 1.0.0 | License - GNU LGPL 3 */
 /*
+  htmltmpl 1.0.0
+
   This library is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
