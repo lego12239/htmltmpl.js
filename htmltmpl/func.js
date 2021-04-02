c21c428f37638838dd479cfc1f9e64a06fe9f6c6
Oleg Nemanov <lego12239@yandex.ru>
2021-04-02 14:00:10 +0300

    func: add TMPL_IFRET
    On apply stage a function with name specified in NAME attribute value
    is called(with specified arguments in ARGS attribute value - like for
    TMPL_FUNC) and if it return value is an object, then it is inserted
    in the data stack and a content between {%TMPL_IFRET%} and {%/TMPL_IFRET%}
    or between {%TMPL_IFRET%} and {%TMPL_ELSE%} is included in a result.
    If a return value is null, then a content between {%TMPL_ELSE%} and
    {%/TMPL_IFRET%} is included instead(or nothing if there is no TMPL_ELSE).

--- htmltmpl/func.js
+++ htmltmpl/func.js
@@ -63,10 +63,78 @@ htmltmpl.prototype.hdlr_func_apply = function(def, tag)
 	}
 }
 
+/**********************************************************************
+ * TMPL_IFRET HANDLERS
+ **********************************************************************/
+htmltmpl.prototype.hdlr_ifret_parse = function(def, attrs)
+{
+	this._s.parse.unshift([]);
+	this._s.priv.unshift([def, attrs]);
+}
+
+htmltmpl.prototype.hdlr_ifret_end_parse = function(def)
+{
+	var priv;
+	var if_, else_;
+
+	priv = this._s.priv.shift();
+	if (!this.is_tag_match(priv[0], def.start_tag))
+		throw("parse err: " + priv[0].name + " was opened, but " +
+		  def.name + " is being closed");
+	if (priv[0].name == "TMPL_ELSE") {
+		else_ = this._s.parse.shift();
+		priv = this._s.priv.shift();
+	}
+
+	if_ = this._s.parse.shift();
+	this._s.parse[0].push([priv[0].name, [ priv[1], if_, else_ ]]);
+}
+
+htmltmpl.prototype.hdlr_ifret_apply = function(def, tag)
+{
+	var attrs;
+	var val, val2;
+	var i;
+
+	if (this.funcs[tag[0].NAME] == null)
+		if (this.p.err_on_no_data)
+			throw("Cann't find func '" + tag[0]["NAME"] + "'.");
+		else
+			return;
+
+	val = this.funcs[tag[0].NAME].apply(this, tag[0].ARGS);
+	if (val == null) {
+		/* else tmpl */
+		if (tag[2] != null)
+			this._apply(tag[2]);
+		return;
+	}
+	if (Array.isArray(val))
+		throw("hdlr_ifret_apply err: returned value should be an object");
+	this._s.v.unshift(Object.assign({}, this._s.v[0], {data_lookup_depth: 2}));
+	this._s.data.unshift(val);
+	this._apply(tag[1]);
+	this._s.data.shift();
+	this._s.v.shift();
+}
+
+
 htmltmpl.prototype.tags["TMPL_FUNC"] = {
 	pfunc: htmltmpl.prototype.hdlr_func_parse,
 	afunc: htmltmpl.prototype.hdlr_func_apply,
 	pafuncs: {ARGS: htmltmpl.prototype._parse_tag_attr_FUNCARGS,
 	  ESCAPE: htmltmpl.prototype._parse_tag_attr_ESCAPE},
 	name: "TMPL_FUNC" };
+htmltmpl.prototype.tags["TMPL_IFRET"] = {
+	pfunc: htmltmpl.prototype.hdlr_ifret_parse,
+	afunc: htmltmpl.prototype.hdlr_ifret_apply,
+	pafuncs: {ARGS: htmltmpl.prototype._parse_tag_attr_FUNCARGS},
+	name: "TMPL_IFRET" };
+htmltmpl.prototype.tags["/TMPL_IFRET"] = {
+	pfunc: htmltmpl.prototype.hdlr_ifret_end_parse,
+	start_tag: [htmltmpl.prototype.tags["TMPL_IFRET"],
+		htmltmpl.prototype.tags["TMPL_ELSE"]],
+	name: "/TMPL_IFRET" };
+
+htmltmpl.prototype.tags["TMPL_ELSE"].start_tag.push(htmltmpl.prototype.tags["TMPL_IFRET"]);
 }
